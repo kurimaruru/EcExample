13a020a6193f2d255954809b5f52ad52a473f809
kurimaru <kurikuribaseball530@gmail.com>
2022-02-13 15:21:52 +0900

    edit

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -28,10 +28,11 @@
                         メールアドレス</th>
                       <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                         作成日</th>
-                      {{-- <th
-                        class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
-                      </th>
                       <th
+                        class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                        編集
+                      </th>
+                      {{-- <th
                         class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                       </th> --}}
                   </thead>
@@ -41,11 +42,11 @@
                       <td class="md:px-4 py-3">{{ $owner->name }}</td>
                       <td class="md:px-4 py-3">{{ $owner->email }}</td>
                       <td class="md:px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
-                      {{-- <td class="md:px-4 py-3">
+                      <td class="md:px-4 py-3">
                         <button onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id ])}}'"
-                          class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded ">編集</button>
+                          class="text-black bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded ">編集</button>
                       </td>
-                      <form id="delete_{{$owner->id}}" method="post"
+                      {{-- <form id="delete_{{$owner->id}}" method="post"
                         action="{{ route('admin.owners.destroy', ['owner' => $owner->id ] )}}">
                         @csrf
                         @method('delete')
