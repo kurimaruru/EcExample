1b48b31b5bd616e4cb39565c86a507c0524e17ea
kurimaru <kurikuribaseball530@gmail.com>
2022-02-14 22:03:50 +0900

    shop

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            AdmimSeeder::class,
+            OwnerSeeder::class,
+            ShopSeeder::class
+        ]);
     }
 }
